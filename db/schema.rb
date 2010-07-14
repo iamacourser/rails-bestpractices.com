de4262d3f7695ff550469c1d121a3b2d9820f70e
Richard Huang <flyerhzm@gmail.com>
2010-07-14 17:16:30 +0800

    use rails_indexes to find missing indexes

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100713040251) do
+ActiveRecord::Schema.define(:version => 20100714091523) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "first_name",       :default => "",    :null => false
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(:version => 20100713040251) do
     t.datetime "updated_at"
   end
 
+  add_index "comments", ["commentable_id", "commentable_type"], :name => "index_comments_on_commentable_id_and_commentable_type"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
   create_table "implementations", :force => true do |t|
     t.integer  "post_id"
     t.integer  "user_id"
@@ -44,6 +47,9 @@ ActiveRecord::Schema.define(:version => 20100713040251) do
     t.datetime "updated_at"
     t.integer  "comments_count", :default => 0
   end
+
+  add_index "implementations", ["post_id"], :name => "index_implementations_on_post_id"
+  add_index "implementations", ["user_id"], :name => "index_implementations_on_user_id"
 
   create_table "pages", :force => true do |t|
     t.string   "name"
@@ -64,6 +70,8 @@ ActiveRecord::Schema.define(:version => 20100713040251) do
     t.integer  "vote_points",    :default => 0
     t.integer  "view_count"
   end
+
+  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
@@ -103,5 +111,8 @@ ActiveRecord::Schema.define(:version => 20100713040251) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "votes", ["post_id"], :name => "index_votes_on_post_id"
+  add_index "votes", ["user_id"], :name => "index_votes_on_user_id"
 
 end
