d7eb7648bbe885d701cee14753d3526d75de5e0b
Richard Huang <flyerhzm@gmail.com>
2010-07-17 18:14:35 +0800

    style for implementation

--- app/models/implementation.rb
+++ app/models/implementation.rb
@@ -4,4 +4,8 @@ class Implementation < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   has_many :comments, :as => :commentable, :dependent => :destroy
+  
+  def belongs_to?(user)
+    user && user_id == user.id
+  end
 end
