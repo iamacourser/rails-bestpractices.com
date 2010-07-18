43db5ac2d05e4955cb8669ebd62818d530c0e703
Richard Huang <flyerhzm@gmail.com>
2010-07-18 23:43:16 +0800

    only tweet on production server

--- lib/tweetable.rb
+++ lib/tweetable.rb
@@ -1,11 +1,13 @@
 module Tweetable
-  def self.included(base)
-    base.class_eval do
-      after_create :async_tweet
+  if Rails.env == 'production'
+    def self.included(base)
+      base.class_eval do
+        after_create :async_tweet
+      end
     end
-  end
 
-  def async_tweet
-    Resque.enqueue(Resque::TweetJob, tweet_content)
+    def async_tweet
+      Resque.enqueue(Resque::TweetJob, tweet_content)
+    end
   end
 end
