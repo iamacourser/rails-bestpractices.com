53c3d4855d0a74a0eedd606eee7e25545305eb6b
NgTzeYang <ngty77@gmail.com>
2010-07-22 15:56:12 +0800

    We don't actually need the before block.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,6 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe User do
 
-  before do
-    Factory.create(:user)
-  end
-
   extend RailsBestPractices::Macros
   should_be_gravastic
   should_act_as_authentic # This affects the validity of a user
