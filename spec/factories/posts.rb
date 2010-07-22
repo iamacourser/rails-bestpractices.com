b1dbb83acd0dab5e1e79c036537f9e99d1532f94
NgTzeYang <ngty77@gmail.com>
2010-07-22 17:01:29 +0800

    Avoid conflicting Post#title.

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,9 @@
 Factory.define :post do |p|
-  p.title 'post'
+  p.sequence(:title) {|n| "Post #{n}" }
   p.body "subject\n=======\ntitle\n-----"
 end
 
 Factory.define :code_post, :parent => :post do |p|
-  p.title 'code post'
+  p.sequence(:title) {|n| "Code Post #{n}" }
   p.body "subject\n=======\ntitle\n-----\n    def test\n      puts 'test'\n    end"
 end
