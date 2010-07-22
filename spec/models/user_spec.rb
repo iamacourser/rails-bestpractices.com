3e3c0857fad90489178ee3edd7c6945c54d6bda1
NgTzeYang <ngty77@gmail.com>
2010-07-22 16:41:20 +0800

    Revised RailsBestPractices::Macro such that instead of extending, user just use 'include'.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe User do
 
-  extend RailsBestPractices::Macros
+  include RailsBestPractices::Macros
   should_be_gravastic
   should_act_as_authentic # This affects the validity of a user
 
