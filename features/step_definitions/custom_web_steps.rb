17c084839136affe36fd6ea6a5098de89add2ddb
NgTzeYang <ngty77@gmail.com>
2010-07-23 17:13:08 +0800

    Added home-baked steps definitions.

---
+++ features/step_definitions/custom_web_steps.rb
@@ -0,0 +1,33 @@
+Then %r{^I should see (success|error) message "([^"]*)"$} do |type, message|
+  type = 'notice' if type == 'success'
+  within("#flash .message.#{type}") do
+    if page.respond_to? :should
+      page.should have_content(message)
+    else
+      assert page.has_content?(message)
+    end
+  end
+end
+
+Then %r{^I should be back on "([^"]*)" page$} do |page_title|
+  within('h2') do
+    if page.respond_to? :should
+      page.should have_content(page_title)
+    else
+      assert page.has_content?(page_title)
+    end
+  end
+end
+
+Then %r{^I should see error fields?:? (.*)$} do |fields|
+  while match = fields.match(/((.*?)"([^"]+)")/)
+    xpath = '//li[contains(@class,"error")]/label[text()="%s"]' % match[3]
+    fields.sub!(match[1],'')
+    if page.respond_to? :should
+      page.should have_xpath(xpath)
+    else
+      assert page.has_xpath?(xpath)
+    end
+  end
+end
+
