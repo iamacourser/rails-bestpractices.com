f420dfc0bd81b396731654ac7f1ba498560c65fa
Richard Huang <flyerhzm@gmail.com>
2010-07-24 00:56:50 +0800

    fix Resque::TweetJob define

--- lib/resque/tweet_job.rb
+++ lib/resque/tweet_job.rb
@@ -1,28 +1,30 @@
-class Resque::TweetJob
+module Resque
+  class TweetJob
 
-  @queue = :tweet
+    @queue = :tweet
 
-  TWIITER_YAML = Rails.root.join('config', 'twitter.yml')
-  TWIITER_CONFIG = YAML.load_file(TWIITER_YAML)[Rails.env] rescue nil
+    TWIITER_YAML = Rails.root.join('config', 'twitter.yml')
+    TWIITER_CONFIG = YAML.load_file(TWIITER_YAML)[Rails.env] rescue nil
 
-  BITLY_YAML = Rails.root.join('config', 'bitly.yml')
-  BITLY_CONFIG = YAML.load_file(BITLY_YAML)[Rails.env] rescue nil
+    BITLY_YAML = Rails.root.join('config', 'bitly.yml')
+    BITLY_CONFIG = YAML.load_file(BITLY_YAML)[Rails.env] rescue nil
 
-  def self.perform(content)
-    title, path = content[:title], content[:path]
-    url = bitly.shorten("http://rails-bestpractices.com/#{path}").short_url
-    twitter.update("#{title} #{url} #railsbp")
+    def self.perform(content)
+      title, path = content[:title], content[:path]
+      url = bitly.shorten("http://rails-bestpractices.com/#{path}").short_url
+      twitter.update("#{title} #{url} #railsbp")
+    end
+
+    def self.twitter
+      config = TWIITER_CONFIG
+      httpauth = Twitter::HTTPAuth.new(config['email'], config['password'])
+      Twitter::Base.new(httpauth)
+    end
+
+    def self.bitly
+      config = BITLY_CONFIG
+      Bitly.new(config['username'], config['api_key'])
+    end
+
   end
-
-  def self.twitter
-    config = TWIITER_CONFIG
-    httpauth = Twitter::HTTPAuth.new(config['email'], config['password'])
-    Twitter::Base.new(httpauth)
-  end
-
-  def self.bitly
-    config = BITLY_CONFIG
-    Bitly.new(config['username'], config['api_key'])
-  end
-
 end
