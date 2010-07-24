47927d6da9450a17b8c81b001379ea507ab18e67
NgTzeYang <ngty77@gmail.com>
2010-07-24 23:54:34 +0800

    Factory should create Post instances with associated user.

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,11 @@
 Factory.define :post do |p|
   p.sequence(:title) {|n| "Post #{n}" }
   p.body "subject\n=======\ntitle\n-----"
+  p.association :user
 end
 
 Factory.define :code_post, :parent => :post do |p|
   p.sequence(:title) {|n| "Code Post #{n}" }
   p.body "subject\n=======\ntitle\n-----\n    def test\n      puts 'test'\n    end"
+  p.association :user
 end
