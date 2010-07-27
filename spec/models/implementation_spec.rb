52a14849772071dcc8f83edf7a3b76d5897fd25e
Richard Huang <flyerhzm@gmail.com>
2010-07-27 13:38:53 +0800

    remove should_be_tweetable spec helper

--- spec/models/implementation_spec.rb
+++ spec/models/implementation_spec.rb
@@ -7,13 +7,6 @@ describe Implementation do
   should_be_user_ownable
   should_have_entries_per_page 10
 
-  should_be_tweetable do |impl|
-    {
-      :title => "Implementation of #{impl.post.title}",
-      :path => "posts/#{impl.post.to_param}/implementation"
-    }
-  end
-
   should_belong_to :post
   should_have_many :comments, :dependent => :destroy
   should_validate_presence_of :body
