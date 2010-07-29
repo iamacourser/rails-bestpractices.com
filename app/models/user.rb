6ce5d7ef178effb2d235815297f0944dd0b07a7a
Richard Huang <flyerhzm@gmail.com>
2010-07-29 23:01:20 +0800

    login with facebook and twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  include ConnectProfile
   acts_as_authentic
   is_gravtastic!
   
@@ -6,4 +7,13 @@ class User < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   has_many :votes, :dependent => :destroy
   has_many :implementations, :dependent => :destroy
+
+  after_create :update_profile
+
+  private
+    def update_profile
+      if self.profile
+        self.update_attribute(:login, self.profile[:name])
+      end
+    end
 end
