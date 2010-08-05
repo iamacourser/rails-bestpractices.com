8e1d41bfff1eb4b6ef0aa2b01d22dc055b0c0136
NgTzeYang <ngty77@gmail.com>
2010-08-05 16:34:19 +0800

    Bug fixing (spelling problem & counter_cache for :commentable).

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,12 +2,12 @@ class Comment < ActiveRecord::Base
 
   include UserOwnable
 
-  belongs_to :commentable, :polymorphic => true, :counter_cache => true, :touch => true
+  belongs_to :commentable, :polymorphic => true, :touch => true
   validates_presence_of :body
   validates_presence_of :username, :if => Proc.new { |comment| !comment.user_id }
 
   scope :post, where(:commentable_type => 'Post').order("created_at desc")
-  scope :implement, where(:commentable_type => 'Implement').order("created_at desc")
+  scope :implement, where(:commentable_type => 'Implementation').order("created_at desc")
 
   def self.per_page
     20
@@ -22,7 +22,8 @@ class Comment < ActiveRecord::Base
     when Post
       "Post #{commentable.title}"
     when Implementation
-      "Implemenation of #{commentable.post.title}"
+      "Implementation of #{commentable.post.title}"
     end
   end
+
 end
